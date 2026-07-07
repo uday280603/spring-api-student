a61f73bea83ef4e7d39507edc3af64742791708b
uday <udayindrawar123@gmail.com>
2026-07-07 15:08:35 +0530

    Update CORS config to allow all origins

--- src/main/java/com/ro/RoSpringBootApi/Config/CorsConfig.java
+++ src/main/java/com/ro/RoSpringBootApi/Config/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.ro.RoSpringBootApi.Config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
